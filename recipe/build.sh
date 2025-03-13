0a9d318e8bfc8fcfbb728f722864614f9a52ea8a
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-13 01:59:38 -0700

    update to 1.19.0

---
+++ recipe/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+go build -buildmode=pie -trimpath -modcacherw -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
+go-licenses save . --save_path=license-files --ignore github.com/hashicorp/vault --ignore github.com/aliyun/aliyun-oss-go-sdk --ignore github.com/couchbase/goprotostellar
