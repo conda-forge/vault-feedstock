69fdd1722efbe2e1077f2cceeac405f34fd31123
Daniel Nachun <daniel.nachun@gmail.com>
2025-03-13 11:31:04 -0700

    fix license issues

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,3 +4,4 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 go build -buildmode=pie -trimpath -modcacherw -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
 go-licenses save . --save_path=license-files --ignore github.com/hashicorp/vault --ignore github.com/aliyun/aliyun-oss-go-sdk --ignore github.com/couchbase/goprotostellar
+cp -r ${RECIPE_DIR}/license-files/* ${SRC_DIR}/license-files
